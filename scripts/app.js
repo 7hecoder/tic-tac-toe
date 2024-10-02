4e8b200c73f668b0290fe96c261780031c64ecc1
7hecoder <7hecoder@proton.me>
2024-10-02 18:18:49 +0530

    Javascript added

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+const playerConfigOverlay = document.getElementById('config-overlay');
+const backdrop = document.getElementById('backdrop');
+
+const editPlayer1 = document.getElementById('edit-player1');
+const editPlayer2 = document.getElementById('edit-player1');
+const cancelConfig = document.getElementById('cancel-config');
+
+editPlayer1.addEventListener('click', openPlayerConfig);
+editPlayer2.addEventListener('click', openPlayerConfig);
+
+cancelConfig.addEventListener('click', closePlayerConfig);
+backdrop.addEventListener('click', closePlayerConfig)
