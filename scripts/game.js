c68f424226042a6bb06403593d79cba216432257
7hecoder <7hecoder@proton.me>
2024-10-07 12:28:45 +0530

    Logic Added-Game winning

--- scripts/game.js
+++ scripts/game.js
@@ -1,41 +1,91 @@
-function startGame(){
-    if(players[0].name === '' || players[1].name === ''){
-        alert('Please set custom player for both players');
-        return;
-    }
-    activePlayerName.textContent = players[activePlayer].name;
-    gameArea.style.display = 'block';
-
+function startGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom player for both players");
+    return;
+  }
+  activePlayerName.textContent = players[activePlayer].name;
+  gameArea.style.display = "block";
 }
 
-function switchPlayer(){
-    if (activePlayer === 0){
-        activePlayer = 1;
-    } else {
-        activePlayer = 0;
-    }
-    activePlayerName.textContent = players[activePlayer].name;
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerName.textContent = players[activePlayer].name;
 }
 
+function selectGameField(event) {
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1; //-1 convert it to number
+  const selectedRow = selectedField.dataset.row - 1;
 
-function selectGameField(event){
-    const selectedField = event.target;
-    const selectedColumn = selectedField.dataset.col - 1; //-1 convert it to number
-    const selectedRow = selectedField.dataset.row - 1;
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field");
+    return;
+  }
 
-    if (gameData[selectedRow][selectedColumn] > 0){
-        alert('Please select an empty field');
-        return;
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  const winnerID = checkForGameOver();
+  console.log(winnerID);
+
+  currentRound++;
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  //checking the rows for equality
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
     }
+  }
 
-    selectedField.textContent = players[activePlayer].symbol;
-    selectedField.classList.add('disabled');
+  //checking the columns for equality
 
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
 
-    gameData[selectedRow][selectedColumn] = activePlayer + 1;
-    console.log(gameData);
+  //Diagonal: Top left to bottom right
 
-    switchPlayer();
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
 
+  //Diagonal: Bottom left to top right
 
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9){ //for no winner
+    return -1;
+  }
+
+  return 0;
 }
